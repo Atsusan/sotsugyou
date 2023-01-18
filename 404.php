beebebadb4538d14eaa574a1981b6bbcca406d15
Atsusan <as2k_abc@hotmail.com>
2023-01-19 04:00:00 +0900

    change 404

--- 404.php
+++ 404.php
@@ -1,7 +1,10 @@
 <?php
 /*
-Template Name: 404
+Template Name: 404ページ
 */
 get_header(); ?>
-<p>404ページ</p>
+    <div class="container-404">
+        <p class="title-404">404ページ</p>
+        <p class="text-404">お探しのページは見つかりませんでした。</p>
+    </div>
 <?php get_footer(); ?>
