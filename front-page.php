ed80331c1e6562bd594eec30b13f1a50bc24e8e7
Atsusan <as2k_abc@hotmail.com>
2023-01-25 17:48:23 +0900

    front-page.php change style

--- front-page.php
+++ front-page.php
@@ -8,16 +8,16 @@
         </div>
         <div class="inner">
             <div class="about-head flex">
-                <div class="about-titles">
+                <div class="about-titles wow fadeInUp">
                     <h2 class="about-title section-title">about&nbsp;us</h2>
                     <p class="about-sub section-sub">当社について</p>
                 </div><!-- /.about-titles -->
-                <a href="<?php bloginfo('url'); ?>/about" class="more-wh is-pc"><span class="more-arrow-wh is-pc"></span>View
+                <a href="<?php bloginfo('url'); ?>/about" class="more-wh is-pc wow fadeInUp"><span class="more-arrow-wh is-pc"></span>View
                     more</a>
             </div>
             <!-- /.about-head -->
             <div class="about-body flex">
-                <div class="about-text-box">
+                <div class="about-text-box wow fadeInUp">
                     <p class="about-text">
                         急速に広がったグローバル社会に対応できる人材を育成することで、文化・言語の垣根を越えたコミュニケーションを活発にし、一人でも多くの人が豊かに暮らせる世界を実現することを使命とする。
                     </p>
@@ -27,9 +27,9 @@
                 <!-- /.about-text-box -->
                 <!-- /.about-text -->
                 <div class="about-footer">
-                    <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/img-company.jpg" alt="img-company.jpg">
+                    <img class="wow fadeInUp" src="<?php echo esc_url(get_template_directory_uri()); ?>/img/img-company.jpg" alt="img-company.jpg">
                     <div class="about-footer-link">
-                        <a href="<?php bloginfo('url'); ?>/about" class="more-wh display-hidden-active"><span class="more-arrow-wh display-hidden-active"></span>View more</a>
+                        <a href="<?php bloginfo('url'); ?>/about" class="more-wh display-hidden-active wow fadeInUp"><span class="more-arrow-wh display-hidden-active"></span>View more</a>
                     </div>
                     <div class="about-background-right-sp"></div>
                 </div>
@@ -46,7 +46,7 @@
     <section id="service" class="service section-background-skew service-background-skew">
         <div class="service-head-container inner">
             <div class="service-head flex">
-                <div class="service-section-titles">
+                <div class="service-section-titles wow fadeInUp">
                     <h2 class="service-section-title section-title">service</h2>
                     <div class="service-sub section-sub">サービス</div>
                 </div>
@@ -60,21 +60,21 @@
         <div class="inner">
             <ul class="service-list">
                 <li class="service-item">
-                    <div class="service-item-head">
+                    <div class="service-item-head wow fadeInUp">
                         <div class="service-item-numbertag">01</div>
                         <div class="service-item-img service-item-img-01">
                             <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/img-service01.jpg" alt="">
                         </div>
                         <h3 class="service-subtitle"><span>ビジネス英語研修</span></h3>
                     </div>
-                    <div class="service-item-body">
+                    <div class="service-item-body wow fadeInUp">
                         <p class="service-text service-text-01">
                             ビジネス英会話はこれからの時代、すべてのビジネスパーソンが学ぶべき必須スキルと考えおります。海外にビジネス展開する際にはもちろんのこと、日本国内でも英会話コミュニケーションができることによってチャンスが掴める場面があります。
                         </p>
                     </div>
                 </li>
                 <li class="service-item">
-                    <div class="service-item-head">
+                    <div class="service-item-head wow fadeInUp">
                         <div class="service-item-numbertag">02</div>
                         <div class="service-item-img service-item-img-02">
                             <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/img-service02.jpg" alt="">
@@ -84,13 +84,13 @@
                             <h3 class="service-subtitle-2"><span>コミュニケーション研修</span></h3>
                         </div>
                     </div>
-                    <div class="service-item-body">
+                    <div class="service-item-body wow fadeInUp">
                         <p class="service-text service-text-02">急速にグローバル化が進んでおり、ビジネスの場面に限らず様々な文化的背景を持つ者同士の交流はもはや日常的な光景となりました。<br>
                             言語や文化が異なる相手を理解することで世界が広がり、新たなビジネスチャンスに巡り会うことは少なくありません。</p>
                     </div>
                 </li>
                 <li class="service-item">
-                    <div class="service-item-head">
+                    <div class="service-item-head wow fadeInUp">
                         <div class="service-item-numbertag">03</div>
                         <div class="service-item-img service-item-img-03">
                             <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/img-service03.jpg" alt="">
@@ -100,7 +100,7 @@
                             <h3 class="service-subtitle-2"><span>サポートプログラム</span></h3>
                         </div>
                     </div>
-                    <div class="service-item-body">
+                    <div class="service-item-body wow fadeInUp">
                         <p class="service-text service-text-03">
                             将来的に海外で働きたい方に向けた講座をご用意しております。一般的には3ヶ月〜1年の期間で基本的な英会話スキルと、海外でのビジネスマナー習得を目指します。<br>
                             通常の語学留学では得られないビジネスの場で通用するコミュニケーションスキル習得に重点をおいておりますので、海外でビジネス展開する際に自信を持って活動することができるようになります。
@@ -110,7 +110,7 @@
             </ul>
             <!-- /.service-list -->
             <div class="service-footer">
-                <a href="<?php bloginfo('url'); ?>/service" class="more-bl display-hidden-active"><span class="more-arrow-bl display-hidden-active"></span>View more</a>
+                <a href="<?php bloginfo('url'); ?>/service" class="more-bl display-hidden-active wow fadeInUp"><span class="more-arrow-bl display-hidden-active"></span>View more</a>
             </div>
         </div>
         <!-- /.inner -->
@@ -122,17 +122,17 @@
     <section id="casestudy" class="casestudy section">
         <div class="inner">
             <div class="casestudy-head flex">
-                <div class="casestudy-titles">
+                <div class="casestudy-titles wow fadeInUp">
                     <h2 class="casestudy-title section-title">case&nbsp;study</h2>
                     <div class="casestudy-sub section-sub">導入事例</div>
                 </div>
                 <!-- /.casestudy-titles -->
-                <a href="<?php bloginfo('url'); ?>/case" class="more-wh is-pc"><span class="more-arrow-wh is-pc"></span>View&nbsp;more</a>
+                <a href="<?php bloginfo('url'); ?>/case" class="more-wh is-pc wow fadeInUp"><span class="more-arrow-wh is-pc"></span>View&nbsp;more</a>
             </div>
             <!-- /.casestudy-head flex -->
             <ul class="casestudy-list">
 
-                <li class="casestudy-item">
+                <li class="casestudy-item wow fadeInUp">
                     <a href="<?php bloginfo('url'); ?>/case/#training" class="casestudy-item-link">
                         <div class="casestudy-item-head">
                             <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/logo/img-case01.jpg" alt="">
@@ -150,7 +150,7 @@
                     <!-- /.casestudy-item-link -->
                 </li>
 
-                <li class="casestudy-item">
+                <li class="casestudy-item wow fadeInUp">
                     <a href="<?php bloginfo('url'); ?>/case/#communication" class="casestudy-item-link">
                         <div class="casestudy-item-head">
                             <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/logo/img-case02.jpg" alt="">
@@ -167,7 +167,7 @@
                     </a>
                     <!-- /.casestudy-item-link -->
                 </li>
-                <li class="casestudy-item">
+                <li class="casestudy-item wow fadeInUp">
                     <a href="<?php bloginfo('url'); ?>/case/#program" class="casestudy-item-link">
                         <div class="casestudy-item-head">
                             <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/logo/img-case03.jpg" alt="">
@@ -184,7 +184,7 @@
                     </a>
                     <!-- /.casestudy-item-link -->
                 </li>
-                <li class="casestudy-item">
+                <li class="casestudy-item wow fadeInUp">
                     <a href="<?php bloginfo('url'); ?>/case/#communication" class="casestudy-item-link">
                         <div class="casestudy-item-head">
                             <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/logo/img-case04.jpg" alt="">
@@ -201,7 +201,7 @@
                     </a>
                     <!-- /.casestudy-item-link -->
                 </li>
-                <li class="casestudy-item">
+                <li class="casestudy-ite wow fadeInUp">
                     <a href="<?php bloginfo('url'); ?>/case/#program" class="casestudy-item-link">
                         <div class="casestudy-item-head">
                             <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/logo/img-case05.jpg" alt="">
@@ -218,7 +218,7 @@
                     </a>
                     <!-- /.casestudy-item-link -->
                 </li>
-                <li class="casestudy-item">
+                <li class="casestudy-item wow fadeInUp">
                     <a href="<?php bloginfo('url'); ?>/case/#training" class="casestudy-item-link">
                         <div class="casestudy-item-head">
                             <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/logo/img-case06.jpg" alt="">
@@ -238,7 +238,7 @@
             </ul>
             <!-- /.casestudy-list -->
             <div class="casestudy-footer">
-                <a href="<?php bloginfo('url'); ?>/case" class="more-wh display-hidden-active"><span class="more-arrow-wh display-hidden-active"></span>View more</a>
+                <a href="<?php bloginfo('url'); ?>/case" class="more-wh display-hidden-active wow fadeInUp"><span class="more-arrow-wh display-hidden-active"></span>View more</a>
             </div>
         </div>
         <!-- /.inner -->
@@ -250,11 +250,11 @@
     <section id="news" class="news section section-background-skew news-background-skew">
         <div class="news-inner inner">
             <div class="news-head flex">
-                <div class="news-titles">
+                <div class="news-titles wow fadeInUp">
                     <h2 class="news-section-title section-title">news</h2>
                     <div class="news-section-sub section-sub">新着情報</div>
                 </div>
-                <a href="<?php bloginfo('url'); ?>/news" class="more-bl is-pc"><span class="more-arrow-bl is-pc"></span>View more</a>
+                <a href="<?php bloginfo('url'); ?>/news" class="more-bl is-pc wow fadeInUp"><span class="more-arrow-bl is-pc"></span>View more</a>
             </div>
             <!-- /.news-head flex -->
             <div class="news-body">
@@ -264,7 +264,7 @@
                         <?php while (have_posts()) : ?>
                             <?php the_post(); ?>
 
-                            <li class="news-item">
+                            <li class="news-item wow fadeInUp">
                                 <a href="<?php the_permalink(); ?>" class="news-item-link">
                                     <div class="news-item-head">
                                         <p class="news-item-category">
