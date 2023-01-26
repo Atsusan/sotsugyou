2aa87b309862de49d695e31898a1a970ce1d894c
Atsusan <as2k_abc@hotmail.com>
2023-01-27 00:41:38 +0900

    google fonts

--- functions.php
+++ functions.php
@@ -20,7 +20,7 @@ add_action('after_setup_theme', 'my_setup');
  */
 function my_script_init() {
     // Google Fonts
-    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@1,700&family=Noto+Sans+JP:wght@400;500;700&display=swap');
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Fira+Sans:wght@500;700&family=Noto+Sans+JP:wght@500;700&display=swap');
     // Swiper
     wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css', array(), 8, 'all');
     wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js', array(), 8, true);
