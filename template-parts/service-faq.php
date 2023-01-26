ac8f75a34ee8988d7ee3de5bec85f7f355caf3c6
Atsusan <as2k_abc@hotmail.com>
2023-01-26 14:31:14 +0900

    page-service change style and php

--- template-parts/service-faq.php
+++ template-parts/service-faq.php
@@ -6,12 +6,12 @@
 <?php if(get_field("q-{$count}") && get_field("a-{$count}")) :?>
     <li class="service-nav-item accordion wow fadeInUp">
         <dt class="accordion-head">
-            <p><?php the_field("q-{$count}"); ?></p>
+            <p><?php echo nl2br(get_field("q-{$count}")); ?></p>
             <span class="accordion-icon"></span>
         </dt>
         <dd class="accordion-body">
             <div class="accordion-body-in">
-            <?php the_field("a-{$count}"); ?>
+            <?php echo nl2br(get_field("a-{$count}")); ?>
             </div><!-- /.accordion-body-in -->
         </dd>
 <?php endif; ?>
